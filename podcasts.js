0a33ec5972ee6d6d0ce367c768d95cee2d4a24ae
Franzitschana <zeitsurfer@gmail.com>
2026-03-26 18:00:40 +0100

    Update podcasts.js

--- podcasts.js
+++ podcasts.js
@@ -1,16 +1,17 @@
 const podcasts = [
-  {
-    datum: "2026-03-27",
-    kin: 222,
-    name: "Blauer Affe",
-    titel: "Ruf des Raumes",
-    audio: "https://drive.google.com/file/d/1Z3GG5Czwj49cF4s76slnbKzK9s1bkyWK/preview"
-  },
-  {
-    datum: "2026-03-26",
-    kin: 221,
-    name: "Weißer Hund",
-    titel: "Ruf des Raumes",
-    audio: "https://drive.google.com/file/d/1Z3GG5Czwj49cF4s76slnbKzK9s1bkyWK/preview"
-  }
+  { datum: "2026-03-26", kin: 221, name: "Weißer Hund", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/13XQ8jOvhxrYrW9y5XdOjyXD2jEFzUt_b/preview" },
+  { datum: "2026-03-25", kin: 220, name: "Roter Mond", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/16zjAPpWQXS8FoGflNlKa6R8bCtWnWuWH/preview" },
+  { datum: "2026-03-24", kin: 219, name: "Gelber Stern", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/18GBQXS73OaqkGKNNGiFlghECLdBPx7Ck/preview" },
+  { datum: "2026-03-23", kin: 218, name: "Eismeerblauer Niwanes", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/1AOoJZ39L7up5dWbDox7g8jQxiwV3B1HK/preview" },
+  { datum: "2026-03-22", kin: 217, name: "Blaue Hand", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/1CX7vw4bdMOjLxMkbL2q8UmA-ItlOWyOL/preview" },
+  { datum: "2026-03-21", kin: 216, name: "Weißer Weltenüberbrücker", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/1EPQdbwv5nZU_7hXYv6YGSCuOV9Jqpz59/preview" },
+  { datum: "2026-03-20", kin: 215, name: "Rote Schlange", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/1GpexpVJk9VXmgtV3r-cwVfYa1vbfwNuo/preview" },
+  { datum: "2026-03-19", kin: 214, name: "Gelber Samen", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/1K_iRt1jrklXUhh4zj5-EMtMUPtrQnr8A/preview" },
+  { datum: "2026-03-18", kin: 213, name: "Blaue Nacht", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/1PJr_dSKw6zY-0Vmkk3ZJc2CrIW7xg3MH/preview" },
+  { datum: "2026-03-17", kin: 212, name: "Weißer Wind", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/1WqmDc5232G4SAKSA3E9wcQJHx_uAO0u_/preview" },
+  { datum: "2026-03-16", kin: 211, name: "Blauer Affe", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/1envcUhaK1nFQ1G3M-SvvCOtCJpG9Kg5U/preview" },
+  { datum: "2026-03-15", kin: 210, name: "Gelbe Sonne", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/1iGBwl8hnLvQZh_ci08t0dti-7qNNtoze/preview" },
+  { datum: "2026-03-14", kin: 209, name: "Roter Mond", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/1kLdVmEH1b2pcv3nx9q9xKAcOyGi3fMlb/preview" },
+  { datum: "2026-03-13", kin: 208, name: "Weißer Hund", titel: "Ruf des Raumes", audio: "https://drive.google.com/file/d/1m-x7uwPyifcSo8eh2nI2M_3ZQV5j35Dn/preview" }
 ];
+                                                                                                                                                                                                                                                                               
