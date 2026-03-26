8342986679cfaa688dd9d10b36969ef42f8ed533
Franzitschana <zeitsurfer@gmail.com>
2026-03-26 11:31:44 +0100

    Create podcasts.js

---
+++ podcasts.js
@@ -0,0 +1,9 @@
+const podcasts = [
+  {
+    datum: "2026-03-26",
+    kin: 221,
+    name: "Weißer Hund",
+    titel: "Ruf des Raumes",
+    audio: "https://drive.google.com/file/d/1Z3GG5Czwj49cF4s76slnbKzK9s1bkyWK/preview"
+  }
+];
