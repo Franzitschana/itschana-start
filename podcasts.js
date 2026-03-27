e479de5c43f84fb8eacb6de385842d7520a63f9a
Franzitschana <zeitsurfer@gmail.com>
2026-03-27 15:29:08 +0100

    Update podcasts.js

--- podcasts.js
+++ podcasts.js
@@ -1,11 +1,11 @@
 const podcasts = [
   {
-    datum: "2026-03-27",
-    kin: 222,
-    name: "Blauer Affe",
-    titel: "Ruf des Raumes",
-    audio: "https://drive.google.com/file/d/1Z3GG5Czwj49cF4s76slnbKzK9s1bkyWK/preview"
-  },
+  datum: "2026-03-27",
+  kin: 222,
+  name: "Blauer Affe",
+  titel: "Ruf des Raumes",
+  audio: "https://drive.google.com/file/d/1oR6JGbMt7UvAFJTBebEOEz5hel42qjwX/preview"
+}
   {
     datum: "2026-03-26",
     kin: 221,
